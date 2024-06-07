00adb5696293f28689346e9653f5c11161b7aec8
ignacioDorigo <71302004+ignacioDorigo@users.noreply.github.com>
2024-06-07 20:48:43 -0300

    cambios hasta aca funca

--- mimuni/src/main/java/com/example/demo/MimuniApplication.java
+++ mimuni/src/main/java/com/example/demo/MimuniApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,39 +10,70 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.demo.modelo.Barrio;
 import com.example.demo.modelo.Vecino;
+import com.example.demo.modelo.Vecinoregistrado;
 import com.example.demo.repository.*;
 
 @SpringBootApplication
-public class MimuniApplication implements CommandLineRunner{
-	
+public class MimuniApplication implements CommandLineRunner {
+
 	@Autowired
 	BarrioRepository repobarrio;
+
 	@Autowired
 	VecinoRepository repovecino;
-	
+
+	@Autowired
+	VecinoregistradoRepository repovecinoregistrado;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MimuniApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		//mostrarBarrios();
-		mostrarVecinos();
-		
+//		mostrarBarrios();
+//		mostrarVecinos();
+		register("DNI28000429", "ignaciodorigo@nacho0305sadasd.com.ar");
+		mostrarVecinosRegistrados();
+
 	}
-	
+
 	public void mostrarBarrios() {
 		List<Barrio> barrios = repobarrio.findAll();
-		for(Barrio barrio : barrios) {
+		for (Barrio barrio : barrios) {
 			System.out.println(barrio);
 		}
 	}
-	
-	
+
 	public void mostrarVecinos() {
 		List<Vecino> vecinos = repovecino.findAll();
-		for(Vecino vecino : vecinos) {
+		for (Vecino vecino : vecinos) {
 			System.out.println(vecino);
+		}
+	}
+
+	public void mostrarVecinosRegistrados() {
+//		System.out.println("Vecinos registrados");
+		List<Vecinoregistrado> vecinos = repovecinoregistrado.findAll();
+		for (Vecinoregistrado vecino : vecinos) {
+			System.out.println(vecino);
+		}
+	}
+
+	public void register(String documento, String mail) {
+		Optional<Vecinoregistrado> vecinoYaRegistrado = repovecinoregistrado.findById(documento);
+		if (vecinoYaRegistrado.isPresent()) {
+			System.out.println("YA ESTAS REGISTRADO");
+		} else {
+			Optional<Vecino> vecinoOptional = repovecino.findById(documento);
+			if (vecinoOptional.isPresent()) {
+				String contrasenia = documento;
+				Vecinoregistrado nuevoVecino = new Vecinoregistrado(documento, mail, contrasenia, "B", "S");
+				repovecinoregistrado.save(nuevoVecino);
+				System.out.println("Registro exitoso");
+			} else {
+				System.out.println("No puede registrarse xq no es vecino");
+			}
 		}
 	}
 
---
+++ mimuni/src/main/java/com/example/demo/modelo/Vecinoregistrado.java
@@ -0,0 +1,87 @@
+package com.example.demo.modelo;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vecinosregistrados")
+public class Vecinoregistrado {
+	
+	@Id
+	private String documento;
+	
+	@Basic
+	
+	private String mail;
+	@Basic
+	private String contrasenia;
+	@Basic
+	private String estado;
+	
+	@Column(name="primeringreso")
+	private String primerIngreso;
+	
+	public Vecinoregistrado() {
+		
+	}
+
+	public Vecinoregistrado(String documento, String mail, String contrasenia, String estado, String primerIngreso) {
+		super();
+		this.documento = documento;
+		this.mail = mail;
+		this.contrasenia = contrasenia;
+		this.estado = estado;
+		this.primerIngreso = primerIngreso;
+	}
+
+	public String getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getPrimerIngreso() {
+		return primerIngreso;
+	}
+
+	public void setPrimerIngreso(String primerIngreso) {
+		this.primerIngreso = primerIngreso;
+	}
+
+	@Override
+	public String toString() {
+		return "Vecinoregistrado [documento=" + documento + ", mail=" + mail + ", contrasenia=" + contrasenia
+				+ ", estado=" + estado + ", primerIngreso=" + primerIngreso + "]";
+	}
+	
+
+}
